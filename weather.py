eef3b1727840c5a19c9be4fc89e666b617b6a4ca
bkina <boubacarkina1@gmail.com>
2025-08-09 02:50:06 +0100

    meteo..

--- weather.py
+++ weather.py
@@ -9,4 +9,20 @@ def get_current_weather(city="Kansas City"):
 
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
 
-    
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+if __name__ == "__main__":
+    print('\n*** Get Current Weather conditions ***\n')
+
+    city = input("\nPlease enter a city name: ")
+
+    # check for emptystrings or string with only spaces
+    if not bool(city.strip):
+        city = "Kansas City"
+
+    weather_city = get_current_weather(city)
+
+    print("\n")
+    print(weather_city)
